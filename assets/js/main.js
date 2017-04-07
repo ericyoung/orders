ee786148e8ea6aa23f633487205303b41a27c0f3
Eric Young <eric@Erics-MacBook.local>
2017-04-07 17:40:30 -0500

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,18 @@
 		xxsmall: '(max-width: 360px)'
 	});
 
+	(function() {
+	  var links = document.links;
+	  for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+	    // can also be
+	    //  links[i].hostname != 'subdomain.example.com'
+	    if (links[i].hostname != window.location.hostname) {
+	      links[i].target = '_blank';
+	      links[i].className += ' externalLink';
+	    }
+	  }
+	})();
+
 	/**
 	 * Applies parallax scrolling to an element's background image.
 	 * @return {jQuery} jQuery object.
